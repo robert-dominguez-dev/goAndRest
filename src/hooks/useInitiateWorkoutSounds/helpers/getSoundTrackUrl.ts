cd535e6289682fec2755bcebbbe498cd8bda6c53
Robert Dominguez <robert.dominguez@partnersbanka.cz>
2026-02-26 22:20:36 +0100

    Fix countdown files

--- src/hooks/useInitiateWorkoutSounds/helpers/getSoundTrackUrl.ts
+++ src/hooks/useInitiateWorkoutSounds/helpers/getSoundTrackUrl.ts
@@ -1,15 +1,19 @@
 import sample from 'lodash/sample';
 import { ExtendedWorkoutSoundFilePath } from '../../../assets/types.ts';
 
+const checkIsExtended = (
+  paths: string | ExtendedWorkoutSoundFilePath,
+): paths is ExtendedWorkoutSoundFilePath =>
+  typeof paths === 'object' && !!paths.standard;
+
 export const getSoundTrackUrl = (
   oneOrStructuredFilePaths: string | ExtendedWorkoutSoundFilePath,
   isPreferredShort?: boolean,
 ): string | undefined => {
-  if (typeof oneOrStructuredFilePaths === 'string') {
-    return oneOrStructuredFilePaths;
+  if (checkIsExtended(oneOrStructuredFilePaths)) {
+    const { short, standard } = oneOrStructuredFilePaths;
+    return isPreferredShort && short ? short : sample(standard);
   }
 
-  const { short, standard } = oneOrStructuredFilePaths;
-
-  return isPreferredShort ? short : sample(standard);
+  return oneOrStructuredFilePaths;
 };
