5ed4899a86adab1606fa6ad0191860308a5af495
Robert Dominguez <robert.dominguez@partnersbanka.cz>
2026-03-01 15:23:14 +0100

    Fix close

--- src/components/navigation/AppNavigator/screens/LandingScreen/components/LandingScreenFooter/hooks/useSaveWorkoutBottomSheet.tsx
+++ src/components/navigation/AppNavigator/screens/LandingScreen/components/LandingScreenFooter/hooks/useSaveWorkoutBottomSheet.tsx
@@ -2,6 +2,7 @@ import { useAppTranslation } from '../../../../../../../../locales/hooks/useAppT
 import { useAppBottomSheet } from '../../../../../../../common/AppBottomSheet/hooks/useAppBottomSheet.tsx';
 import { AppBottomSheetProps } from '../../../../../../../common/AppBottomSheet/AppBottomSheet.tsx';
 import { SaveWorkoutBottomSheetContent } from '../components/SaveWorkoutBottomSheetContent.tsx';
+import { X } from 'lucide-react-native';
 
 export const useSaveWorkoutBottomSheet = () => {
   const t = useAppTranslation();
@@ -10,13 +11,15 @@ export const useSaveWorkoutBottomSheet = () => {
     <SaveWorkoutBottomSheetContent onClose={onClose} />
   );
 
-  const { bottomSheet, handleOpen } = useAppBottomSheet();
+  const { bottomSheet, handleOpen, handleClose } = useAppBottomSheet();
 
   const openSaveWorkoutBottomSheet = () =>
     handleOpen({
       renderContent,
       title: t('screens.landingScreen.saveWorkoutBottomSheet.title'),
       backgroundColorStatus: 'backgroundAlt',
+      AccessoryRightIconComponent: X,
+      onAccessoryRightPress: handleClose,
     });
 
   return { bottomSheet, openSaveWorkoutBottomSheet };
