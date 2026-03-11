75e581a3cbf7b5a211a7bc4c990c09a7a697f0c1
Robert Dominguez <robert.dominguez@partnersbanka.cz>
2026-03-11 23:47:34 +0100

    Fix vibrations and haptic on Android

--- src/components/controls/helpers/triggerHapticFeedback.ts
+++ src/components/controls/helpers/triggerHapticFeedback.ts
@@ -2,6 +2,7 @@ import ReactNativeHapticFeedback, {
   HapticFeedbackTypes,
   HapticOptions,
 } from 'react-native-haptic-feedback';
+import { IS_ANDROID } from '../../../constants/common.ts';
 
 const options: HapticOptions = {
   enableVibrateFallback: false,
@@ -9,5 +10,7 @@ const options: HapticOptions = {
 };
 
 export const triggerHapticFeedback = (
-  feedbackType: HapticFeedbackTypes = HapticFeedbackTypes.selection,
+  feedbackType: HapticFeedbackTypes = IS_ANDROID
+    ? HapticFeedbackTypes.impactLight
+    : HapticFeedbackTypes.selection,
 ) => ReactNativeHapticFeedback.trigger(feedbackType, options);
--- src/helpers/createVibrationPattern.ts
+++ src/helpers/createVibrationPattern.ts
@@ -1,8 +1,10 @@
+import { IS_ANDROID } from '../constants/common.ts';
+
 /**
  * On iOS the vibration duration is fixed,
  * therefore we make it fixed for both platforms...
  */
-export const FIXED_RUN_VIBRATION_MS = 400;
+export const FIXED_RUN_VIBRATION_MS = IS_ANDROID ? 200 : 400;
 export const FIXED_PAUSE_VIBRATION_MS = 200;
 
 export const createVibrationPattern = (times: number): number[] =>
