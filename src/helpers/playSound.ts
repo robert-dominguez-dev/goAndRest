f755af7d2a9672d9a3fc4407c439df3e9ad99fd3
Robert Dominguez <robert.dominguez@partnersbanka.cz>
2026-03-11 21:08:03 +0100

    Fix play sound fn

--- src/helpers/playSound.ts
+++ src/helpers/playSound.ts
@@ -1,7 +1,7 @@
 import TrackPlayer, { AddTrack } from 'react-native-track-player';
 
 import { APP_NAME } from '../constants/common.ts';
-import { SILENCE_SOUND_KEY, SILENCE_SOUND_TRACK, } from '../assets/constants/common.ts';
+import { SILENCE_SOUND_TRACK } from '../assets/constants/common.ts';
 
 export type PlaySoundParams = {
   soundKey: string;
@@ -22,21 +22,12 @@ export const playSound = async ({
   };
 
   try {
+    await TrackPlayer.removeUpcomingTracks();
     await TrackPlayer.load(track);
     await TrackPlayer.play();
 
-    /**
-     * If this is a countdown sound, we add a silence track after it
-     * to ensure that the ducking stays active before the next sound starts playing.
-     * We ensure only one silence track is in the queue at a time.
-     */
     if (isCountdown) {
-      const queue = await TrackPlayer.getQueue();
-      const hasSilence = queue.some(t => t.id === SILENCE_SOUND_KEY);
-
-      if (!hasSilence) {
-        await TrackPlayer.add(SILENCE_SOUND_TRACK);
-      }
+      await TrackPlayer.add(SILENCE_SOUND_TRACK);
     }
   } catch (error) {
     console.error('Audio playback error:', error);
