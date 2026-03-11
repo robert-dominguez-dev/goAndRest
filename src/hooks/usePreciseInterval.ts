9211d1d01ec374fb7855eac39aa06124efc9b09b
Robert Dominguez <robert.dominguez@partnersbanka.cz>
2026-03-11 20:16:44 +0100

    Remove interval correction

--- src/hooks/usePreciseInterval.ts
+++ src/hooks/usePreciseInterval.ts
@@ -1,6 +1,5 @@
 import { DependencyList, useEffect, useRef } from 'react';
 import { ONE_SECOND_MS } from '../constants/common.ts';
-import { countPreciseInterval } from '../helpers/countPreciseInterval.ts';
 
 export const usePreciseInterval = (
   onTick: () => void,
@@ -8,7 +7,6 @@ export const usePreciseInterval = (
   deps: DependencyList,
 ) => {
   const timeoutIdRef = useRef<number | null>(null);
-  const tickCountRef = useRef(0);
 
   useEffect(() => {
     if (timeoutIdRef.current !== null) {
@@ -20,16 +18,8 @@ export const usePreciseInterval = (
     }
 
     const tick = () => {
-      tickCountRef.current += 1;
       onTick();
-
-      const isCorrectionThreshold = tickCountRef.current % 15 === 0;
-
-      const currentInterval = isCorrectionThreshold
-        ? countPreciseInterval()
-        : ONE_SECOND_MS;
-
-      timeoutIdRef.current = setTimeout(tick, currentInterval);
+      timeoutIdRef.current = setTimeout(tick, ONE_SECOND_MS);
     };
 
     tick();
