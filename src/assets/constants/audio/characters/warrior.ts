d3f909920157fbcaffc9b03a108b8c09980685d0
Robert Dominguez <robert.dominguez@partnersbanka.cz>
2026-06-07 10:00:33 +0200

    Add warrior audio lang mutations

--- src/assets/constants/audio/characters/warrior.ts
+++ src/assets/constants/audio/characters/warrior.ts
@@ -71,10 +71,144 @@ const warriorCs: WorkoutSoundFilePaths = {
   },
 };
 
+const warriorEn: WorkoutSoundFilePaths = {
+  [RunningWorkoutPhase.WARMUP]: {
+    short: require('../../../audio/workout/characters/warrior/en/warrior_en_warmup_short.m4a'),
+    standard: [
+      require('../../../audio/workout/characters/warrior/en/warrior_en_warmup_v1.m4a'),
+      require('../../../audio/workout/characters/warrior/en/warrior_en_warmup_v2.m4a'),
+      require('../../../audio/workout/characters/warrior/en/warrior_en_warmup_v3.m4a'),
+    ],
+  },
+  [RunningWorkoutPhase.WORK]: {
+    short: require('../../../audio/workout/characters/warrior/en/warrior_en_work_short.m4a'),
+    standard: [
+      require('../../../audio/workout/characters/warrior/en/warrior_en_work_v1.m4a'),
+      require('../../../audio/workout/characters/warrior/en/warrior_en_work_v2.m4a'),
+      require('../../../audio/workout/characters/warrior/en/warrior_en_work_v3.m4a'),
+    ],
+  },
+  [RunningWorkoutPhase.REST]: {
+    short: require('../../../audio/workout/characters/warrior/en/warrior_en_rest_short.m4a'),
+    standard: [
+      require('../../../audio/workout/characters/warrior/en/warrior_en_rest_v1.m4a'),
+      require('../../../audio/workout/characters/warrior/en/warrior_en_rest_v2.m4a'),
+      require('../../../audio/workout/characters/warrior/en/warrior_en_rest_v3.m4a'),
+    ],
+  },
+  [RunningWorkoutPhase.RECOVERY]: {
+    short: require('../../../audio/workout/characters/warrior/en/warrior_en_recovery_short.m4a'),
+    standard: [
+      require('../../../audio/workout/characters/warrior/en/warrior_en_recovery_v1.m4a'),
+      require('../../../audio/workout/characters/warrior/en/warrior_en_recovery_v2.m4a'),
+      require('../../../audio/workout/characters/warrior/en/warrior_en_recovery_v3.m4a'),
+    ],
+  },
+  [RunningWorkoutPhase.COOLDOWN]: {
+    short: require('../../../audio/workout/characters/warrior/en/warrior_en_cooldown_short.m4a'),
+    standard: [
+      require('../../../audio/workout/characters/warrior/en/warrior_en_cooldown_v1.m4a'),
+      require('../../../audio/workout/characters/warrior/en/warrior_en_cooldown_v2.m4a'),
+      require('../../../audio/workout/characters/warrior/en/warrior_en_cooldown_v3.m4a'),
+    ],
+  },
+  [Countdown.Ten]: require('../../../audio/workout/characters/warrior/en/warrior_en_10.m4a'),
+  [Countdown.Nine]: require('../../../audio/workout/characters/warrior/en/warrior_en_9.m4a'),
+  [Countdown.Eight]: require('../../../audio/workout/characters/warrior/en/warrior_en_8.m4a'),
+  [Countdown.Seven]: require('../../../audio/workout/characters/warrior/en/warrior_en_7.m4a'),
+  [Countdown.Six]: require('../../../audio/workout/characters/warrior/en/warrior_en_6.m4a'),
+  [Countdown.Five]: require('../../../audio/workout/characters/warrior/en/warrior_en_5.m4a'),
+  [Countdown.Four]: require('../../../audio/workout/characters/warrior/en/warrior_en_4.m4a'),
+  [Countdown.Three]: require('../../../audio/workout/characters/warrior/en/warrior_en_3.m4a'),
+  [Countdown.Two]: require('../../../audio/workout/characters/warrior/en/warrior_en_2.m4a'),
+  [Countdown.One]: require('../../../audio/workout/characters/warrior/en/warrior_en_1.m4a'),
+  half: {
+    standard: [
+      require('../../../audio/workout/characters/warrior/en/warrior_en_half_v1.m4a'),
+      require('../../../audio/workout/characters/warrior/en/warrior_en_half_v2.m4a'),
+      require('../../../audio/workout/characters/warrior/en/warrior_en_half_v3.m4a'),
+    ],
+  },
+  finish: {
+    standard: [
+      require('../../../audio/workout/characters/warrior/en/warrior_en_finish_v1.m4a'),
+      require('../../../audio/workout/characters/warrior/en/warrior_en_finish_v2.m4a'),
+      require('../../../audio/workout/characters/warrior/en/warrior_en_finish_v3.m4a'),
+    ],
+  },
+};
+
+const warriorSk: WorkoutSoundFilePaths = {
+  [RunningWorkoutPhase.WARMUP]: {
+    short: require('../../../audio/workout/characters/warrior/sk/warrior_sk_warmup_short.m4a'),
+    standard: [
+      require('../../../audio/workout/characters/warrior/sk/warrior_sk_warmup_v1.m4a'),
+      require('../../../audio/workout/characters/warrior/sk/warrior_sk_warmup_v2.m4a'),
+      require('../../../audio/workout/characters/warrior/sk/warrior_sk_warmup_v3.m4a'),
+    ],
+  },
+  [RunningWorkoutPhase.WORK]: {
+    short: require('../../../audio/workout/characters/warrior/sk/warrior_sk_work_short.m4a'),
+    standard: [
+      require('../../../audio/workout/characters/warrior/sk/warrior_sk_work_v1.m4a'),
+      require('../../../audio/workout/characters/warrior/sk/warrior_sk_work_v2.m4a'),
+      require('../../../audio/workout/characters/warrior/sk/warrior_sk_work_v3.m4a'),
+    ],
+  },
+  [RunningWorkoutPhase.REST]: {
+    short: require('../../../audio/workout/characters/warrior/sk/warrior_sk_rest_short.m4a'),
+    standard: [
+      require('../../../audio/workout/characters/warrior/sk/warrior_sk_rest_v1.m4a'),
+      require('../../../audio/workout/characters/warrior/sk/warrior_sk_rest_v2.m4a'),
+      require('../../../audio/workout/characters/warrior/sk/warrior_sk_rest_v3.m4a'),
+    ],
+  },
+  [RunningWorkoutPhase.RECOVERY]: {
+    short: require('../../../audio/workout/characters/warrior/sk/warrior_sk_recovery_short.m4a'),
+    standard: [
+      require('../../../audio/workout/characters/warrior/sk/warrior_sk_recovery_v1.m4a'),
+      require('../../../audio/workout/characters/warrior/sk/warrior_sk_recovery_v2.m4a'),
+      require('../../../audio/workout/characters/warrior/sk/warrior_sk_recovery_v3.m4a'),
+    ],
+  },
+  [RunningWorkoutPhase.COOLDOWN]: {
+    short: require('../../../audio/workout/characters/warrior/sk/warrior_sk_cooldown_short.m4a'),
+    standard: [
+      require('../../../audio/workout/characters/warrior/sk/warrior_sk_cooldown_v1.m4a'),
+      require('../../../audio/workout/characters/warrior/sk/warrior_sk_cooldown_v2.m4a'),
+      require('../../../audio/workout/characters/warrior/sk/warrior_sk_cooldown_v3.m4a'),
+    ],
+  },
+  [Countdown.Ten]: require('../../../audio/workout/characters/warrior/sk/warrior_sk_10.m4a'),
+  [Countdown.Nine]: require('../../../audio/workout/characters/warrior/sk/warrior_sk_9.m4a'),
+  [Countdown.Eight]: require('../../../audio/workout/characters/warrior/sk/warrior_sk_8.m4a'),
+  [Countdown.Seven]: require('../../../audio/workout/characters/warrior/sk/warrior_sk_7.m4a'),
+  [Countdown.Six]: require('../../../audio/workout/characters/warrior/sk/warrior_sk_6.m4a'),
+  [Countdown.Five]: require('../../../audio/workout/characters/warrior/sk/warrior_sk_5.m4a'),
+  [Countdown.Four]: require('../../../audio/workout/characters/warrior/sk/warrior_sk_4.m4a'),
+  [Countdown.Three]: require('../../../audio/workout/characters/warrior/sk/warrior_sk_3.m4a'),
+  [Countdown.Two]: require('../../../audio/workout/characters/warrior/sk/warrior_sk_2.m4a'),
+  [Countdown.One]: require('../../../audio/workout/characters/warrior/sk/warrior_sk_1.m4a'),
+  half: {
+    standard: [
+      require('../../../audio/workout/characters/warrior/sk/warrior_sk_half_v1.m4a'),
+      require('../../../audio/workout/characters/warrior/sk/warrior_sk_half_v2.m4a'),
+      require('../../../audio/workout/characters/warrior/sk/warrior_sk_half_v3.m4a'),
+    ],
+  },
+  finish: {
+    standard: [
+      require('../../../audio/workout/characters/warrior/sk/warrior_sk_finish_v1.m4a'),
+      require('../../../audio/workout/characters/warrior/sk/warrior_sk_finish_v2.m4a'),
+      require('../../../audio/workout/characters/warrior/sk/warrior_sk_finish_v3.m4a'),
+    ],
+  },
+};
+
 export const warriorSoundPathsByLanguage: WorkoutSoundPathsByLanguage = {
   [SupportedLanguageCode.cs]: warriorCs,
-  [SupportedLanguageCode.en]: warriorCs,
-  [SupportedLanguageCode.sk]: warriorCs,
+  [SupportedLanguageCode.en]: warriorEn,
+  [SupportedLanguageCode.sk]: warriorSk,
 };
 
 export const warriorPreviewPathByLanguage: SoundPathByLanguage = {
